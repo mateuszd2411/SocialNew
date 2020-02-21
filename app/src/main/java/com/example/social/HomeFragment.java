4f08b02eb36b4ce220960df011dffbcffe02f2b6
Mateusz <mateuszd2411@gmail.com>
2020-02-21 14:03:01 +0100

    18

--- app/src/main/java/com/example/social/HomeFragment.java
+++ app/src/main/java/com/example/social/HomeFragment.java
@@ -6,8 +6,13 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,8 +20,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+import android.widget.Toast;
+
+import com.example.social.adapters.AdapterPosts;
+import com.example.social.models.ModelPost;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -25,6 +43,10 @@ import com.google.firebase.auth.FirebaseUser;
 public class HomeFragment extends Fragment {
 
     FirebaseAuth firebaseAuth;
+
+    RecyclerView recyclerView;
+    List<ModelPost> postList;
+    AdapterPosts adapterPosts;
 
 
     public HomeFragment() {
@@ -41,7 +63,99 @@ public class HomeFragment extends Fragment {
 
         firebaseAuth = FirebaseAuth.getInstance();
 
+
+        recyclerView = view.findViewById(R.id.postsRecyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+
+
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        postList = new ArrayList<>();
+
+        loadPosts();
+
         return view;
+    }
+
+    private void loadPosts() {
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Posts");
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                postList.clear();
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+
+                    ModelPost modelPost = ds.getValue(ModelPost.class);
+
+
+                    postList.add(modelPost);
+
+                    adapterPosts = new AdapterPosts(getActivity(), postList);
+
+                    recyclerView.setAdapter(adapterPosts);
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                Toast.makeText(getActivity(), ""+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+
+    private void searchPosts(final String searchQuery){
+
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Posts");
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                postList.clear();
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+
+                    ModelPost modelPost = ds.getValue(ModelPost.class);
+
+
+                    if (modelPost.getpTitle().toLowerCase().contains(searchQuery.toLowerCase()) ||
+                            modelPost.getpDescr().toLowerCase().contains(searchQuery.toLowerCase())){
+
+                        postList.add(modelPost);
+
+                    }
+
+
+                    adapterPosts = new AdapterPosts(getActivity(), postList);
+
+                    recyclerView.setAdapter(adapterPosts);
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                Toast.makeText(getActivity(), ""+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 
 
@@ -69,6 +183,42 @@ public class HomeFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 
         inflater.inflate(R.menu.menu_main, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+
+                if (!TextUtils.isEmpty(s)){
+
+                    searchPosts(s);
+                } else {
+
+                    loadPosts();
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+
+                if (!TextUtils.isEmpty(s)){
+
+                    searchPosts(s);
+                } else {
+
+                    loadPosts();
+                }
+
+
+                return false;
+            }
+        });
+
 
         super.onCreateOptionsMenu(menu, inflater);
     }
